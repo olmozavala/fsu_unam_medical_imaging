4fd2c4bee67aed1e9ab15fdb0bc2fdcd10291b7e
Olmo Zavala Romero <olmozavala@olmuzpc>
2016-04-14 22:50:13 -0400

    Improved template files

--- Image_Processing_Examples/oz_allhist.m
+++ Image_Processing_Examples/oz_allhist.m
@@ -1,6 +1,6 @@
-%% This function computes the histogram of an image in RGB
-
 function [hr hg hb] = oz_allhist(img)
+% OZ_ALLHIST Is a function computes the histogram of an image in RGB
+
     hr = oz_hist(img(:,:,1));
     hg = oz_hist(img(:,:,2));
     hb = oz_hist(img(:,:,3));
---
+++ Template/TemplateFunction.m
@@ -0,0 +1,7 @@
+function output = TemplateFunction(a, b)
+% TEMPLATEFUNCTION It is a template function
+%   output = TemplateFunction(a,b) Is the magic result of doing nothing to a and b
+%   a Is an integer and is required
+%   b Is a boolean and is useless
+
+    output = a;
