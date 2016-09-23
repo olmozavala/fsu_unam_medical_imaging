849229ecf92b3d87e71ca34db57947add31111d9
macbookpro <1234>
2016-09-22 22:44:57 -0400

    Add Function Documentation
    Refactor the code to make it cleaner and add documentation

--- Classification/Eigenbreast/eigenbreast.m
+++ Classification/Eigenbreast/eigenbreast.m
@@ -1,22 +1,25 @@
 function [res]=eigenbreast(stack_all)
+% This function applies an extraction of eign breast to a DCE-MRI image
+% Returns a structure with the Eign Left Breast, the Eign Right Breast 
+% and both Eigh breast together. 
+% stack_all
+%        = 4D array where the first dimmension is a temporal index and
+%          the other 3 dimensions are the regular volume indexes of the
+%          Image
+
+
 %% SETTINGS
-
-
 % Manually fixed parameters
-
-endslice=40;  % 40 is a good value in DCE-MRI database
-minvalue=2;   %
-maxvalue= 100; % Value of intensity. Above this value, all intensities are saturated to this (150 is good value in DCE-MRI database).
-%local=1;
+endslice=40;    % 40 is a good value in DCE-MRI database
+minvalue=2;     % value of intensity. below this value, all intensities are saturated to this
+maxvalue= 100;  % Value of intensity. Above this value, all intensities are saturated to this (150 is good value in DCE-MRI database).
 gaussianS=7;
 gaussianD=2;
 
-
 %% PREPROCESSING 
-
 % Torax segmentation
-[local]=torax_segmentation(stack_all);
-stack_allm=stack_all(:,:,local:end-endslice,:);
+[breastRoi]=torax_segmentation(stack_all);
+stack_allm=stack_all(:,:,breastRoi:end-endslice,:);
  
 clear stack_all
 % Intensity normalization
@@ -25,19 +28,21 @@ stack_allm(stack_allm>=maxvalue)=maxvalue; % high value intensity saturation
 stack_allm=stack_allm/maxvalue;            % Range between 1 and 0 
 
 % Spatial smoothing
-[s1 s2 s3 s4]= size(stack_allm);
+[s1, s2, ~, ~]= size(stack_allm);
 midvalue=floor(s2/2);
+
+%Split the image in two asuming simetry: 
 stack_allR=stack_allm(:,midvalue+1:end,:,:); % Right breast
 stack_allL=stack_allm(:,1:midvalue,:,:);     % Left breast
 
+smoothedR=zeros(size(stack_allR));
+smoothedL=zeros(size(stack_allL));
 for i=1:s1, 
     smoothedR(i,:,:,:)=smooth3(squeeze(stack_allR(i,:,:,:)),'gaussian',gaussianS,gaussianD); 
     smoothedL(i,:,:,:)=smooth3(squeeze(stack_allL(i,:,:,:)),'gaussian',gaussianS,gaussianD); 
 end
  
 %% EIGENBREAST EXTRACTION
-
-
 stack_allm(:,midvalue+1:end,:,:)=smoothedR;
 stack_allm(:,1:midvalue,:,:)=smoothedL;
 
@@ -61,19 +66,7 @@ end
 function [local]=torax_segmentation(stack_all)
 mimg=mean(stack_all);
 mimg=squeeze(mimg)/max(mimg(:));
-%level=graythresh(squeeze(mean(mimg(:,:,end-3:end),3)));
-%imint=im2bw(mimg(:,:,end-2),level);
-% se=strel('disk',10);
-% imintcl=imclose(imint,se);
-% BW=imfill(imintcl,'holes');
-% for i=1:170
-% for j=1:5
-% im=squeeze(stack_all(j,:,:,i));
-% im(BW)=0;
-% stack_allm(j,:,:,i)=im;
-% end
-% end
 mimgsag=squeeze(mean(mimg(175:185,:,:)));
 a=sum(mimgsag,2);
-local=find(a>1,1,'last');%[~,local]=max(a);
+local=find(a>1,1,'last');
 end
